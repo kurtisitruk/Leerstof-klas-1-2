ccf8e7749129aef2a1dda43765c776c287ce2fa0
Frank Salliau <frank.salliau@gmail.com>
2024-05-07 11:24:33 +0200

    Les 2

--- Square-oplossing/src/be/odisee/SquareMath.java
+++
@@ -1,9 +0,0 @@
-package be.odisee; //niet nodig
-
-public class SquareMath {
-    static int square(int x) {
-        return x*x; }
-    static double squareRoot(int x) {
-        return Math.sqrt(x);
-    }
-}
